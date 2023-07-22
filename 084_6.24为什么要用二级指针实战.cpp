5e056432ffc9308176cb6d24b00409c1f731b593
xielantu <669616212@qq.com>
2023-07-22 21:28:44 +0800

    Update 084_6.24为什么要用二级指针实战.cpp

--- 084_6.24为什么要用二级指针实战.cpp
+++ 084_6.24为什么要用二级指针实战.cpp
@@ -1,11 +1,103 @@
 ﻿// 084_6.24为什么要用二级指针实战.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+//int* getPosPerson(int pos, int(*pstu)[4])//pstu 是一个指向长度为4的一维 int 数组的指针。
+//{
+//    int* p=NULL;
+//    p = (int*)(pos + pstu);
+//    return p;
+//}
+
+ 
+
+//int* getPosPerson(int pos, int(*pstu)[4], int totalRows)//pstu 是一个指向长度为4的一维 int 数组的指针。
+//{                                                               //使用了 totalRows 来验证 pos 的有效性，确保不会越界访问
+//    if (pos < 0 || pos >= totalRows || pstu == NULL) {
+//        return NULL;  // 参数无效，返回空指针
+//        exit(-1);
+//    }
+//
+//    int* p = *(pstu + pos); // 使用指针算术访问正确的内存位置
+//                            //使用指针算术：避免对指针进行不必要的类型转换和数值运算。
+//                            //可以使用指针算术来访问正确的内存位置。例如，可以使用 pstu + pos 来获取指向指定位置的指针，而无需进行类型转换。
+//    return p;
+//}
+
+//void getPosPerson(int pos, int(*pstu)[4], int totalRows,int** ppos)
+//{                                                        
+//    if (pos < 0 || pos >= totalRows || pstu == NULL) 
+//    {
+//        exit(-1);
+//    }
+//    *ppos = *(pstu + pos);
+//
+//}
+//
+//
+//
+//int main()
+//{
+//    int scores[3][4] = {
+//        {55,66,77,88},
+//        {66,55,99,100},
+//        {11,22,33,59}
+//    };
+//
+//    int* ppos = NULL;
+//    int pos = 0;
+//    printf("请输入你需要看到学生号数0，1，2\n");
+//    scanf_s("%d", &pos);
+//
+//    getPosPerson(pos, scores,4,&ppos);
+//    for (int i = 0; i < 4; i++) 
+//    {
+//        printf("%d ", *ppos++);
+//
+//
+//    }
+//
+//    return 0;
+//}
+
+
+
+void getPosPerson(int pos, int(*pstu)[4], int totalRows, int** ppos)
+{
+    if (pos < 0 || pos >= totalRows || pstu == NULL)
+    {
+        printf("Invalid position or NULL pointer.\n“无效的位置或空指针。”\n");
+        *ppos = NULL;  // 设置ppos为NULL，表示错误条件
+        return;
+    }
+
+    *ppos = *(pstu + pos);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int scores[3][4] = {
+        {55, 66, 77, 88},
+        {66, 55, 99, 100},
+        {11, 22, 33, 59}
+    };
+
+    int* ppos;  // 声明为int*，不需要初始化为NULL
+    int pos = 0;
+    printf("请输入你需要看到学生号数0，1，2\n");
+    scanf_s("%d", &pos);
+
+    getPosPerson(pos, &scores[0], 3, &ppos);
+    if (ppos != NULL)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            printf("%d ", *ppos++);
+        }
+    }
+
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
